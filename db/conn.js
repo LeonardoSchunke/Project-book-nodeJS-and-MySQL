dec0bf1135955ee04c05600817bebb34cadfea20
Leo <leoschunke@hotmail.com>
2022-04-06 09:36:04 -0300

    atualização

--- db/conn.js
+++ db/conn.js
@@ -2,10 +2,10 @@ const mysql = require('mysql')
 
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host:'localhost',
-    user: 'root',
-    password: '',
-    database: 'nodemysql'
+    host:'ec2-34-197-84-74.compute-1.amazonaws.com',
+    user: 'cbqftjurcemztw',
+    password: '50c50f0a7289b1d3d2c96fdfe5019e7786d3b923066d741b6ea5b169b829001e',
+    database: 'd7oliqkis9rcn3'
 })
 
 module.exports = pool
